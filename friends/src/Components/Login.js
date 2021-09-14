33d903c0534ab3e58cf85a2807e963f29f52a949
jMisch44 <jennimisch@gmail.com>
2021-09-14 15:28:18 -0400

    login component completed

--- friends/src/Components/Login.js
+++ friends/src/Components/Login.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+const initialState = {
+  credentials: {
+    username: "",
+    password: "",
+  },
+};
 
 const Login = () => {
-  return <div>hi</div>;
+  const [input, setInput] = useState(initialState);
+  const handleChange = (e) => {
+    setInput({
+      credentials: {
+        ...input.credentials,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:5000/api/login`, input.credentials)
+      .then((res) => {
+        localStorage.setItem("token", res.data.payload);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          username
+          <input
+            type="text"
+            name="username"
+            value={input.credentials.username}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          password
+          <input
+            type="password"
+            name="password"
+            value={input.credentials.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
